c0062b91b1399aecde2f0e160e555fadd2c4686d
RitoMandell <matthew.ryan.mandell@gmail.com>
2016-01-15 12:36:35 -0600

    Merge branch 'master' of https://github.com/RitoMandell/Mandell_4_Game.git
    # Conflicts:
    #	README.md

---
+++ src/matthew_mandell_game/Blocked.java
@@ -0,0 +1,25 @@
+package matthew_mandell_game;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mmandell
+ */
+public class Blocked {
+   
+
+    public static boolean[][] blocked;
+
+    public static boolean[][] getblocked() {
+
+        return blocked;
+
+    }
+
+};
+
